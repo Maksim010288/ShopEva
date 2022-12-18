3a48524ae3648d93db623152d9764bafa5cf2814
Maksim <mysik.bazyk@gmail.com>
2022-12-18 20:04:47 +0200

    creating a test on the ProductService.class

--- src/test/java/com/company/service/ProductServiceTest.java
+++ src/test/java/com/company/service/ProductServiceTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.company.service;
+
+import com.company.entity.ProductEntity;
+import com.company.model.ProductModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class ProductServiceTest {
-  
+
+    @Test
+    void convert() {
+        ProductService service = new ProductService();
+        ProductEntity entity = new ProductEntity("Gilet", "Mac 3");
+
+        ProductModel model = service.convert(entity);
+
+        Assertions.assertEquals(entity.getName(), model.getName());
+        Assertions.assertEquals(entity.getDescription(), model.getDescription());
+    }
 }
